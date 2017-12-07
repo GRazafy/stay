dc459e79a1ab820877ef5ee994e16e9a0a39e5ce
Razafindratsima Gabriel <gabriel.razafindratsima@etu.u-bordeaux.fr>
2017-12-07 22:21:38 +0100

    ajout De classe Reponse

---
+++ ProjetFolder/src/Application/Reponse.java
@@ -0,0 +1,9 @@
+package Application;
+
+public class Reponse {
+    final int TAILLEDIALOGUE = 5;
+    final int NBREPONSE = 3;
+
+    String[][] tabReponse = new String[TAILLEDIALOGUE][NBREPONSE];
+
+}
